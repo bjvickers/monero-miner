84f75e68c2ab6348bcfefe5cf5eb1384a161de05
thefarang <benjamin.vickers@yahoo.co.uk>
2017-12-18 20:21:28 +0100

    working

--- src/bin/startup.js
+++ src/bin/startup.js
@@ -6,7 +6,7 @@ const CoinHive = require('coin-hive');
 (async () => {
 
   log.info({}, 'Creating miner...')
-  const miner = await CoinHive('I11K3gMRbeA1AM1ndWV2ZaMKPTvvLfzj');
+  const miner = await CoinHive('I11K3gMRbeA1AM1ndWV2ZaMKPTvvLfzj')
   log.info({}, 'Miner created.')
 
   log.info({}, 'Starting miner...')
@@ -16,11 +16,19 @@ const CoinHive = require('coin-hive');
   // Listen on events
   miner.on('found', () => log.info({}, 'Found!'))
   miner.on('accepted', () => log.info({}, 'Accepted!'))
-  miner.on('update', data => log.info({}, `
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `));
+
+  let counter = 0
+  miner.on('update', (data) => {
+    counter++
+    if (counter > 10) {
+      log.info({}, `
+        Hashes per second: ${data.hashesPerSecond}
+        Total hashes: ${data.totalHashes}
+        Accepted hashes: ${data.acceptedHashes}
+      `)
+      counter = 0
+    }
+  });
 
   // Stop miner
   // setTimeout(async () => await miner.stop(), 60000);
